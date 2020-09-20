c1e421fb9de00f3c5c7f8df2d53c3d404d5fed15
bacionBOI <58794828+bacionBOI@users.noreply.github.com>
2020-09-20 07:44:13 -0400

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,8 +1,11 @@
-const DeleteThread = require("./DeleteThread")
+const DeleteThread = require("./Admin/DeleteThread")
 const loginWithCookie = require("./loginWithCookie")
-const RemoveUser = require("./RemoveUser")
-const GetUserInfo = require("./GetUserInfo")
-const GetUserGroups = require("./GetUserGroups")
+const RemoveUser = require("./Admin/RemoveUser")
+const GetUserInfo = require("./User/GetUserInfo")
+const GetUserGroups = require("./User/GetUserGroups")
+const GetUserBadges = require("./User/GetUserBadges")
+const UpdateCatory = require("./Admin/UpdateCatory")
+const UpdateThread = require("./Admin/UpdateThread")
 
 class Client {
     constructor(){
@@ -26,8 +29,20 @@ class Client {
     RemoveUser(options){
         return RemoveUser(options)
     }
+    UpdateCatory(opt){
+        return UpdateCatory(opt)
+    }
+    GetUserBadges(username){
+        return GetUserBadges(username).then(function(res){
+            return res
+        })
+    }
+    UpdateCatory(catID){
+        return UpdateCatory(catID)
+    }
+    UpdateThread(tID){
+        return UpdateThread(tID)
+    }
 }
-
-
 
 module.exports = Client
